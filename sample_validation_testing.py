42aa84a506a3a4ebe45b105128219aae8cb385d5
Ho Ying Cheng <faustina.cheng@columbia.edu>
2024-12-11 06:45:34 -0500

    Fix bug

--- sample_validation_testing.py
+++ sample_validation_testing.py
@@ -21,7 +21,7 @@ def trajectory_sample(seed):
     return q_rand
 
 
-with open("sample_validation_testing_output.txt", "w", newline="") as f:
+with open("sample_validation_testing_output.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerow(
         [
@@ -60,53 +60,3 @@ with open("sample_validation_testing_output.txt", "w", newline="") as f:
                 writer.writerow(
                     [module.__class__.__name__, obstacle, time_taken, num_runs]
                 )
-
-
-# class RRT:
-#     def __init__(self):
-#         self.current_obstacle = "NONE"
-
-#     def step(self, start, end, step_size):
-#         return 1
-
-#     def is_state_valid(self, q_seg):
-#         return False
-
-#     def validation_worker(self, i, q_start, q_end, step_size, stop_event):
-#         if stop_event.is_set():
-#             return
-#         q_seg = self.step(q_start, q_end, step_size=i * step_size)
-#         if not self.is_state_valid(q_seg):
-#             stop_event.set()
-
-
-#     def is_segment_valid(self, q_start, q_end):
-
-#         threads = []
-#         stop_event = threading.Event()
-
-#         if self.current_obstacle == 'NONE':
-#             step_size = 0.05
-#         elif self.current_obstacle == 'SIMPLE':
-#             step_size = 0.02
-#         elif self.current_obstacle == 'HARD':
-#             step_size = 0.01
-#         elif self.current_obstacle == 'SUPER':
-#             step_size = 0.005
-
-#         direction = [qr - qn for qr, qn in zip(q_end, q_start)]
-#         length = math.sqrt(sum([d**2 for d in direction]))
-#         num_segs = int(length / step_size)
-#         for i in range(num_segs + 1):
-#             thread = threading.Thread(target=self.validation_worker, args=(i, q_start, q_end, step_size, stop_event))
-#             threads.append(thread)
-#             thread.start()
-#             if stop_event.is_set():
-#                 return False
-
-#         for thread in threads:
-#             thread.join()
-#         return not stop_event.is_set()
-
-# a = RRT()
-# print(a.is_segment_valid([1], [2]))
